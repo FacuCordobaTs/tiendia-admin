6507cce3b2df6abdbe9e9f2200c26487221b9ad1
Facundo Cordoba <facucordoba200@gmail.com>
2025-03-26 18:10:39 -0300

    Solved admindashboard type problem

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -11,7 +11,7 @@ export interface Product {
     name: string;
     description: string;
     price: number;
-    sizes?: string[];
+    sizes?: { size: string; stock: number; }[];
 }
 
 export default function AdminDashboard() {
